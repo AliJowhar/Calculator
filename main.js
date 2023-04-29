3bfbbb6e2335abb920dca4ec385a2fcd86d96998
Ali Jowhar <alijowharcoding@gmail.com>
2023-04-29 13:16:53 -0600

    added a populateDisplay function to display the numbers pressed

--- main.js
+++ main.js
@@ -4,41 +4,15 @@ const clearBtn = document.getElementById("clear");
 const deleteBtn = document.getElementById("delete");
 const numberBtns = document.querySelectorAll("button");
 
-const firstNumber = 0;
-const operator = "";
-const secondNumber = 0;
-
-numberBtns.forEach(function (btns) {
-  btns.addEventListener("click", function () {
-    if (btns.id === "btn-0") {
-      display.textContent = "Button 0 ";
-    } else if (btns.id === "btn-1") {
-      console.log("Button 1 clicked!");
-    } else if (btns.id === "btn-2") {
-      console.log("Button 2 clicked!");
-    } else if (btns.id === "btn-3") {
-      console.log("Button 3 clicked!");
-    } else if (btns.id === "btn-4") {
-      console.log("Button 4 clicked!");
-    } else if (btns.id === "btn-5") {
-      console.log("Button 5 clicked!");
-    } else if (btns.id === "btn-6") {
-      console.log("Button 6 clicked!");
-    } else if (btns.id === "btn-7") {
-      console.log("Button 7 clicked!");
-    } else if (btns.id === "btn-8") {
-      console.log("Button 8 clicked!");
-    } else if (btns.id === "btn-9") {
-      console.log("Button 9 clicked!");
-    }
-  });
-});
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
 
 // basic math operators
 
 // addition
-const add = function (numbers) {
-  return numbers.reduce((total, num) => total + num, 0);
+const add = function (a, b) {
+  return a + b;
 };
 
 // subtract
@@ -47,12 +21,29 @@ const subtract = function (a, b) {
 };
 
 // multiply
-const multiply = function (array) {
-  return array.length
-    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
-    : 0;
+const multiply = function (a, b) {
+  return a * b;
 };
 
 const divide = function (a, b) {
   return a / b;
 };
+
+const operate = function (num1, num2, operator) {
+  return operator(num1, num2);
+};
+
+// Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+const populateDisplay = function () {
+  numberBtns.forEach(function (btns) {
+    btns.addEventListener("click", function () {
+      const currentDisplalyValue =
+        document.getElementById("screen").textContent;
+      const newDisplayValue = currentDisplalyValue + btns.textContent;
+
+      display.textContent = newDisplayValue;
+    });
+  });
+};
+populateDisplay();
