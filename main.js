f39270c2037ab0782af3ef4aad5be7cd3cf49aca
Ali Jowhar <alijowharcoding@gmail.com>
2023-04-30 11:53:25 -0600

    added events to all the non-buttons and made them operate depending on the button clicked

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 "use strict";
 const display = document.getElementById("screen");
+const numberBtns = document.querySelectorAll("#btn-num");
 const clearBtn = document.getElementById("clear");
 const deleteBtn = document.getElementById("delete");
-const numberBtns = document.querySelectorAll("#btn-num");
-const equals = document.getElementById("equal");
+const divideBtn = document.getElementById("divide");
+const multiplyBtn = document.getElementById("multiply");
+const minusBtn = document.getElementById("minus");
+const plusBtn = document.getElementById("plus");
+const equalsBtn = document.getElementById("equal");
 
 let firstNumber = null;
 let operator = null;
@@ -27,6 +31,8 @@ const divide = function (a, b) {
   return a / b;
 };
 const operate = function (num1, num2, operator) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   return operator(num1, num2);
 };
 
@@ -37,12 +43,58 @@ const populateDisplay = function () {
         document.getElementById("screen").textContent;
       const newDisplayValue = currentDisplalyValue + btns.textContent;
       display.textContent = newDisplayValue;
-
-      if (btns.textContent == "+") {
-        firstNumber = newDisplayValue;
-        console.log(firstNumber);
-      }
     });
   });
 };
 populateDisplay();
+
+plusBtn.addEventListener("click", function () {
+  firstNumber = "";
+  operator = "";
+
+  firstNumber += display.textContent;
+  operator = add;
+
+  display.textContent = "";
+});
+
+minusBtn.addEventListener("click", function () {
+  firstNumber = "";
+  operator = "";
+
+  firstNumber += display.textContent;
+  operator = subtract;
+
+  display.textContent = "";
+});
+
+multiplyBtn.addEventListener("click", function () {
+  firstNumber = "";
+  operator = "";
+
+  firstNumber += display.textContent;
+  operator = multiply;
+
+  display.textContent = "";
+});
+divideBtn.addEventListener("click", function () {
+  firstNumber = "";
+  operator = "";
+
+  firstNumber += display.textContent;
+  operator = divide;
+
+  display.textContent = "";
+});
+
+equalsBtn.addEventListener("click", function () {
+  secondNumber = "";
+  secondNumber += display.textContent;
+  console.log(`First number: ${firstNumber}`);
+  console.log(`Second number: ${secondNumber}`);
+
+  if (firstNumber)
+    display.textContent = operate(firstNumber, secondNumber, operator);
+  firstNumber = "";
+  secondNumber = "";
+});
